d86334d011194ae746178b595211b55cc6bfbb50
Andre Vera <andrevera.pe@gmail.com>
2021-12-30 09:54:38 -0500

    first

---
+++ heros.js
@@ -0,0 +1,54 @@
+const table_hero = document.querySelector('#row-heros');
+//const table_hero = document.getElementById('row-heros');
+
+//showHeros();
+showPokemons();
+async function showHeros() {
+    const response = await fetch("backend/superheros/get-heros.php", {
+      method: "get",
+      headers: {
+        "Content-Type": "aplication/json",
+      },
+    });
+    const heros = await response.json();
+  
+    let filas = "";
+    for (let hero of heros) {
+      filas += `
+              <tr>
+                  <td>${hero.image}</td>                 
+                  <td>${hero.hero_name}</td>
+                  <td>${hero.actor_name}</td>
+                  <td>${hero.nation}</td>
+                  <td>${hero.cell_phone}</td>
+              </tr>
+          `;
+    }
+    table_hero.innerHTML = filas;
+  };
+
+  async function showPokemons() {
+    console.log('empezando');
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10", {
+      method: "get",
+      headers: {
+        "Content-Type": "aplication/json",
+      },
+    });
+    console.log('esperando respuesta', response);
+    const heros = await response.json();
+    console.log('respuesta obtenida', heros);
+    let filas = "";
+    for (let hero of heros.results) {
+      filas += `
+              <tr>
+                  <td></td>                 
+                  <td>${hero.name}</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+              </tr>
+          `;
+    }
+    table_hero.innerHTML = filas;
+  };
